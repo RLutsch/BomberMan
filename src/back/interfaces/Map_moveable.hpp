f68670cdca334b5706399a3b3f5c885efca9e0cb
Vern01 <verdunw2@gmail.com>
2017-07-30 13:10:09 +0200

    commented on function move in interfaces/Map_moveable.hpp

--- src/back/interfaces/Map_moveable.hpp
+++ src/back/interfaces/Map_moveable.hpp
@@ -1,4 +1,4 @@
 class Map_moveable {
   virtual void update(int &map) = 0;//checking if move is possible. Lets bots move and taking user movement
-  virtual void move() = 0;
+  virtual void move() = 0;//change through states
 };
