f8d4ace8e243284fa98c68d41b63efba4c7ef1cd
Vern01 <verdunw2@gmail.com>
2017-07-31 17:27:16 +0200

    started with the factory and added lost map

---
+++ src/back/factories/Object_factory.cpp
@@ -0,0 +1,6 @@
+#include "Object_factory.hpp"
+
+Map_object Object_factory::create_object(std::string id) {
+  if (!id.compare("A"))
+    return ;
+}
---
+++ src/back/factories/Object_factory.hpp
@@ -0,0 +1,6 @@
+#include "../abstracts/Map_object.hpp"
+
+class Object_factory {
+public:
+  Map_object  create_object(std::string id);
+};
